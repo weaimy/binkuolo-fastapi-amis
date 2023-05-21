0b49405bc401fec6ded5416bf16a1e2322f0eb57
weaimy <tangguipeng@126.com>
2023-05-22 07:38:35 +0800

    the 24th commit

--- api/api.py
+++ api/api.py
@@ -6,7 +6,7 @@
 """
 from fastapi import APIRouter
 from api.endpoints.test import test_oath2
-from api.endpoints import user, role, access, websocket, upload, category, pages, article, content
+from api.endpoints import user, role, access, websocket, upload, category, pages, article, content, job
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.post("/test/oath2", tags=["测试oath2授权"])(test_oath2)
@@ -19,4 +19,5 @@ api_router.include_router(category.router, prefix='/admin', tags=["栏目管理"
 api_router.include_router(pages.router, prefix='/admin', tags=["页面管理"])
 api_router.include_router(article.router, prefix='/admin', tags=["文章管理"])
 api_router.include_router(content.router, prefix='/admin', tags=["内容管理"])
+api_router.include_router(job.router, prefix='/admin', tags=["招聘管理"])
 
